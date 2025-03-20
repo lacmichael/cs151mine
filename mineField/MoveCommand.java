f3dc2963f6f801a526894146aac024b9c212857a
jonahwongg <jonah.wong@sjsu.edu>
2025-03-19 17:54:17 -0700

    Update MoveCommand.java

--- mineField/MoveCommand.java
+++ mineField/MoveCommand.java
@@ -1,5 +1,23 @@
 package mineField;
 
-public class MoveCommand {
-    
+import mvc.Command;
+import mvc.Model;
+
+public class MoveCommand extends Command {
+    private Heading heading;
+
+    public MoveCommand(Model model, Heading heading) {
+        super(model);
+        this.heading = heading;
+    }
+
+    @Override
+    public void execute() {
+        Field field = (Field) model;
+        int currentRow = field.getPlayerRow();
+        int currentCol = field.getPlayerCol();
+
+        int newRow = currentRow + heading.getRowChange();
+        int newCol = currentCol + heading.getColChange();
+    }
 }
